435dbbc123c6dac826e18fe797736ce72e69b5c3
Tim <89969636+SwaggyPotter@users.noreply.github.com>
2023-05-31 21:57:27 +0200

    Faster jump

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -63,7 +63,7 @@ class MovableObject extends DrawableObject {
                 this.y -= this.speedY
                 this.speedY -= this.acceleration
             }
-        }, 1000 / 25)
+        }, 22)
     }
 
 
